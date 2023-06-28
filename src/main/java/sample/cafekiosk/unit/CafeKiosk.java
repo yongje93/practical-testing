5fd9a7031829d4e1700f6d91182e41e4cbeb2f60
Yongje Shin <yongje1211@gmail.com>
2023-06-28 22:06:26 +0900

    :sparkles: TDD: Test Driven Development

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -39,11 +39,9 @@ public class CafeKiosk {
     }
 
     public int calculateTotalPrice() {
-        int totalPrice = 0;
-        for (Beverage beverage : beverages) {
-            totalPrice += beverage.getPrice();
-        }
-        return totalPrice;
+        return beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
     }
 
     public Order createOrder() {
@@ -64,5 +62,4 @@ public class CafeKiosk {
 
         return new Order(currentDateTime, beverages);
     }
-
 }
