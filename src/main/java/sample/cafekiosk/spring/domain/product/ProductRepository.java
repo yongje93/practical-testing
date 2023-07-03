14642c98583a7d642891d64e654ab0d545d891c6
Yongje Shin <yongje1211@gmail.com>
2023-07-03 15:30:51 +0900

    :art: selling_type -> selling_status

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -11,7 +11,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     /**
      * select *
      * from product
-     * where selling_type in ('SELLING', 'HOLD');
+     * where selling_status in ('SELLING', 'HOLD');
      */
-    List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingTypes);
+    List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingStatuses);
 }
